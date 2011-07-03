2f2a15fd346622ebfa41fe951a4c49a65a5e2b06
Mike De La Loza <mikedll@mikedll.com>
2011-07-02 22:53:28 -0700

    Adding odesk req signing in prep for polling hours off of ODesk.

--- run.rb
+++ run.rb
@@ -3,12 +3,34 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
-
 require 'active_support/all'
+require 'digest/md5'
 
+API_KEY = ENV['ODESK_API_KEY']
+API_SECRET = ENV['ODESK_API_SECRET']
+ODESK_ROOT = 'https://www.odesk.com/'
+
+
+def params_signed( params )
+  raise "Need a key and secret, but couldn't find them in environment." if API_KEY.blank? or API_SECRET.blank?
+
+  params.merge!(:api_key => API_KEY)
+  params.merge!(:api_sig => 
+                Digest::MD5.hexdigest(API_SECRET.to_s + params.sort { |a,b| a.first <=> b.first }.flatten.join.to_s))
+end
+
+def hours
+  query = "SELECT week_worked_on, hours, earnings, charges, agency_id WHERE provider_id = AND week_worked_on = '2011-06-27'"
+  provider_id = "~~b7c77f857ebb0450"
+  params = params_signed(:tq => query, :tqx => 'out:json')
+  RestClient.get "#{ODESK_ROOT}/gds/timereports/v1/providers/#{provider_id}/hours.xml", :params => params
+  doc = Nokogiri::XML( response )
+  puts doc
+  []
+end
 
 def webservice_jobs( params )
-  response = RestClient.get 'https://www.odesk.com/api/profiles/v1/search/jobs.xml', :params => params
+  response = RestClient.get '#{ODESK_ROOT}/api/profiles/v1/search/jobs.xml', :params => params
 
   doc = Nokogiri::XML( response )
   jobs = doc.css('jobs job').map do |job|
@@ -66,4 +88,7 @@ def showjobs
 end
 
 
-showjobs
+# showjobs
+
+hours
+
